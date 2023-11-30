f1099aacc018c3d4733e29d1fae43767ccd2fab2
Rajtilak Pal <rajtilakls2510@gmail.com>
2023-11-30 23:19:57 +0530

    Added State copy with nogil. Had to restructure the whole state format

--- ludobackendc/ludoc.py
+++ ludobackendc/ludoc.py
@@ -4,7 +4,7 @@ from cython.cimports.libc.stdlib import free, calloc
 
 @cython.cclass
 class GameConfig:
-    n_players: cython.short
+    n_players = cython.declare(cython.short, visibility='public')
     player_colours: cython.p_short
     colour_player: cython.short[5]
 
@@ -109,35 +109,92 @@ class LudoModel:
 Block = cython.struct(pawns=cython.short, pos=cython.short, rigid=cython.bint)
 
 
+StateStruct = cython.struct(
+    n_players=cython.short,
+    game_over=cython.bint,
+    current_player=cython.short,
+    num_more_moves=cython.short,
+    dice_roll=cython.short,
+    last_move_id=cython.short,
+    pawn_pos=cython.p_short,
+    num_blocks=cython.short,
+    all_blocks=Block[16]
+)
+
+
+@cython.cfunc
+def create_new_state(n_players: cython.short) -> StateStruct:
+    pawn_pos: cython.p_short = cython.cast(cython.p_short, calloc(n_players * 93, cython.sizeof(cython.short)))
+    state: StateStruct = StateStruct(n_players=n_players, game_over=False, current_player=0, num_more_moves=0, dice_roll=0, last_move_id=0, pawn_pos=pawn_pos, num_blocks=0)
+    i: cython.Py_ssize_t
+    for i in range(16):
+        state.all_blocks[i] = Block(pawns=0, pos=cython.cast(cython.short, i), rigid=(i % 2 == 0))
+    return state
+
+
+@cython.cfunc
+def free_state(state: StateStruct):
+    free(state.pawn_pos)
+
+
+@cython.cfunc
+@cython.nogil
+@cython.exceptval(check=False)
+def copy_state(state: StateStruct) -> StateStruct:
+    new_state: StateStruct = state
+    pawn_pos: cython.p_short = cython.cast(cython.p_short, calloc(state.n_players * 93, cython.sizeof(cython.short)))
+    i: cython.Py_ssize_t
+    for i in range(state.n_players * 93):
+        pawn_pos[i] = state.pawn_pos[i]
+    new_state.pawn_pos = pawn_pos
+    return new_state
+
+
 @cython.cclass
 class State:
-    config: GameConfig
-    game_over: cython.bint
-    current_player: cython.short
-    num_more_moves: cython.short
-    dice_roll: cython.short
-    last_move_id: cython.short
-    pawn_pos: cython.p_short
-    num_blocks: cython.short
-    all_blocks: Block[16]
+    state_struct: StateStruct
 
-    def __init__(self, config: GameConfig):
-        self.config = config
-        self.game_over = False
-        self.current_player = 0
-        self.num_more_moves = 0
-        self.dice_roll = 0
-        self.last_move_id = 0
-        self.pawn_pos = cython.cast(cython.p_short, calloc(self.config.n_players * 93, cython.sizeof(cython.short)))
-        self.num_blocks = 0
+    def __init__(self, n_players=0):
+        self.state_struct: StateStruct = create_new_state(n_players)
+
+    def set(self, state_dict):
+        mappings = {
+            "pawn": ["", "R1", "R2", "R3", "R4", "G1", "G2", "G3", "G4", "Y1", "Y2", "Y3", "Y4", "B1", "B2", "B3",
+                     "B4"],
+            "pos": ["", "RB1", "RB2", "RB3", "RB4", "GB1", "GB2", "GB3", "GB4", "YB1", "YB2", "YB3", "YB4", "BB1",
+                    "BB2", "BB3", "BB4"]
+                   + [f"P{i + 1}" for i in range(52)]
+                   + ["RH1", "RH2", "RH3", "RH4", "RH5", "RH6", "GH1", "GH2", "GH3", "GH4", "GH5", "GH6", "YH1", "YH2",
+                      "YH3", "YH4", "YH5", "YH6", "BH1", "BH2", "BH3", "BH4", "BH5", "BH6"]
+        }
+        dice_roll = 0
+        roll = reversed(state_dict["dice_roll"])
+        for r in roll:
+            dice_roll = dice_roll * 7 + r
+
+        pawn_pos = cython.cast(cython.p_short, calloc(state_dict['n_players'] * 93, cython.sizeof(cython.short)))
+        for key in state_dict.keys():
+            if "Player" in key:
+                player_num = int(key[-1])
+                for pawn, pos in state_dict[key].items():
+                    pawn = mappings["pawn"].index(pawn)
+                    pos = mappings["pos"].index(pos)
+                    pawn_pos[player_num * 93 + pos] = pawn_pos[player_num * 93 + pos] * 17 + pawn
+        state: StateStruct = StateStruct(n_players=state_dict["n_players"], game_over=state_dict["game_over"],
+                                         current_player=state_dict["current_player"],
+                                         num_more_moves=state_dict["num_more_moves"],
+                                         dice_roll=dice_roll, last_move_id=state_dict["last_move_id"],
+                                         pawn_pos=pawn_pos, num_blocks=len(state_dict["all_blocks"]))
         i: cython.Py_ssize_t
-        for i in range(16):
-            self.all_blocks[i] = Block(pawns=0, pos=cython.cast(cython.short, i), rigid=(i % 2 == 0))
+        for i in range(state.num_blocks):
+            pawns = 0
+            for p in state_dict["all_blocks"][i]["pawns"]:
+                pawns = pawns * 17 + mappings["pawn"].index(p)
+            state.all_blocks[i] = Block(pawns=pawns, pos=mappings["pos"].index(state_dict["all_blocks"][i]["pos"]),
+                                        rigid=state_dict["all_blocks"][i]["rigid"])
+        self.state_struct = state
 
-    def __dealloc__(self):
-        free(self.pawn_pos)
-
-    def set(self, state, config=None):
+    def get(self):
         mappings = {
             "pawn": ["", "R1", "R2", "R3", "R4", "G1", "G2", "G3", "G4", "Y1", "Y2", "Y3", "Y4", "B1", "B2", "B3",
                      "B4"],
@@ -147,67 +204,26 @@ class State:
                    + ["RH1", "RH2", "RH3", "RH4", "RH5", "RH6", "GH1", "GH2", "GH3", "GH4", "GH5", "GH6", "YH1", "YH2",
                       "YH3", "YH4", "YH5", "YH6", "BH1", "BH2", "BH3", "BH4", "BH5", "BH6"]
             }
-        if config:
-            self.config = config
-        self.game_over = state["game_over"]
-        self.current_player = state["current_player"]
-        self.num_more_moves = state["num_more_moves"]
-        self.dice_roll = 0
-        roll = reversed(state["dice_roll"])
-        for r in roll:
-            self.dice_roll = self.dice_roll * 7 + r
-        self.last_move_id = state["last_move_id"]
-        free(self.pawn_pos)
-        self.pawn_pos = cython.cast(cython.p_short, calloc(self.config.n_players * 93, cython.sizeof(cython.short)))
-        for key in state.keys():
-            if "Player" in key:
-                player_num = int(key[-1])
-                for pawn, pos in state[key].items():
-                    pawn = mappings["pawn"].index(pawn)
-                    pos = mappings["pos"].index(pos)
-                    self.pawn_pos[player_num * 93 + pos] = self.pawn_pos[player_num * 93 + pos] * 17 + pawn
-        self.num_blocks = len(state["all_blocks"])
-        i: cython.Py_ssize_t
-        for i in range(self.num_blocks):
-            pawns = 0
-            for p in state["all_blocks"][i]["pawns"]:
-                pawns = pawns * 17 + mappings["pawn"].index(p)
-            self.all_blocks[i] = Block(pawns=pawns, pos=mappings["pos"].index(state["all_blocks"][i]["pos"]), rigid=state["all_blocks"][i]["rigid"])
-
-        # print(self.game_over, self.current_player,self.num_more_moves, self.dice_roll, self.last_move_id, self.num_blocks)
-        # j: cython.Py_ssize_t
-        # for i in range(self.config.n_players):
-        #     for j in range(93):
-        #         print(self.pawn_pos[i * 93 + j], end=" ")
-        #     print("")
-        # block: Block
-        # for block in self.all_blocks:
-        #     print(block)
-
-    def get(self):
-        mappings = {"pawn": ["", "R1", "R2", "R3", "R4", "G1", "G2", "G3", "G4", "Y1", "Y2", "Y3", "Y4", "B1", "B2", "B3", "B4"],
-                    "pos": ["","RB1", "RB2", "RB3", "RB4", "GB1", "GB2", "GB3", "GB4", "YB1", "YB2", "YB3", "YB4","BB1", "BB2", "BB3", "BB4"]
-                        + [f"P{i+1}" for i in range(52)]
-                        + ["RH1", "RH2", "RH3", "RH4", "RH5", "RH6", "GH1", "GH2", "GH3", "GH4", "GH5", "GH6", "YH1", "YH2", "YH3", "YH4", "YH5", "YH6", "BH1", "BH2", "BH3", "BH4", "BH5", "BH6"]
-                    }
         state = dict()
-        state["game_over"] = self.game_over
-        state["current_player"] = self.current_player
-        state["num_more_moves"] = self.num_more_moves
+        s: StateStruct = self.state_struct
+        state["n_players"] = s.n_players
+        state["game_over"] = s.game_over
+        state["current_player"] = s.current_player
+        state["num_more_moves"] = s.num_more_moves
         state["dice_roll"] = []
-        roll: cython.short = self.dice_roll
+        roll: cython.short = s.dice_roll
         while roll != 0:
             state["dice_roll"].append(roll % 7)
             roll //= 7
-        state["last_move_id"] = self.last_move_id
+        state["last_move_id"] = s.last_move_id
         player: cython.short
-        for player in range(self.config.n_players):
+        for player in range(s.n_players):
             k = dict()
             state[f"Player {player}"] = k
             pos: cython.short
             for pos in range(1, 93):
                 pawns = []
-                pawn: cython.short = self.pawn_pos[player * 93 + pos]
+                pawn: cython.short = s.pawn_pos[player * 93 + pos]
                 while pawn != 0:
                     pawns.append(mappings["pawn"][pawn % 17])
                     pawn //= 17
@@ -215,19 +231,28 @@ class State:
                     k[p] = mappings["pos"][pos]
         state["all_blocks"] = []
         i: cython.Py_ssize_t
-        for i in range(self.num_blocks):
+        for i in range(s.num_blocks):
             pawns = []
-            pawn: cython.short = self.all_blocks[i].pawns
+            pawn: cython.short = s.all_blocks[i].pawns
             while pawn != 0:
                 pawns.append(mappings["pawn"][pawn % 17])
                 pawn //= 17
-            state["all_blocks"].append({"pawns": pawns, "pos": mappings["pos"][self.all_blocks[i].pos], "rigid": self.all_blocks[i].rigid})
+            state["all_blocks"].append(
+                {"pawns": pawns, "pos": mappings["pos"][s.all_blocks[i].pos], "rigid": s.all_blocks[i].rigid})
         return state
+
+    def get_copy(self):
+        new_state: State = State()
+        new_state.state_struct = copy_state(self.state_struct)
+        return new_state
+
+    def __dealloc__(self):
+        free_state(self.state_struct)
 
 
 @cython.cclass
 class Ludo:
-    state: State
+    state: StateStruct
     model: LudoModel
     winner: cython.short
 
--- ludobackendc/main.py
+++ ludobackendc/main.py
@@ -4,14 +4,15 @@ try:
     config = ludoc.GameConfig([["red", "yellow"], ["green", "blue"]])
     model = ludoc.LudoModel(config=config)
     # print(model.config)
-    state = ludoc.State(config=config)
+    state = ludoc.State(config.n_players)
 
-    state.set({"game_over": False, "current_player": 0, "num_more_moves": 1, "dice_roll": [6, 2], "last_move_id": 0,
+    state.set({"n_players": config.n_players, "game_over": False, "current_player": 0, "num_more_moves": 1, "dice_roll": [6, 2], "last_move_id": 0,
                "Player 0": {"R1": "RB1", "R2": "P5", "R3": "P46", "R4": "RH6", "Y1": "YB4", "Y2": "P2", "Y3": "P30", "Y4": "P30"},
                "Player 1": {"B1": "BB1", "B2": "P6", "B3": "P47", "B4": "BH5", "G1": "GH1", "G2": "P3", "G3": "P31", "G4": "P31"},
                "all_blocks": [{"pawns": ["Y3", "Y4"], "pos": "P30", "rigid": False},
                               {"pawns": ["G3", "G4"], "pos": "P31", "rigid": True}]
                })
     print(state.get())
+    print(state.get_copy().get())
 except Exception as e:
     print(e)
